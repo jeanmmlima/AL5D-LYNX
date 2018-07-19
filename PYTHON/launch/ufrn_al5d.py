9c4fd0918379278543de1ffa28c866ce324692aa
Jean Mário <jean@dca.ufrn.br>
2018-07-19 15:16:06 -0300

    outras atualizacoes e modificaoes

--- PYTHON/launch/ufrn_al5d.py
+++ PYTHON/launch/ufrn_al5d.py
@@ -15,6 +15,10 @@ import sys
 class RoboticArmAL5D:
     """Driver do Braco Robotico AL5D"""
     #1. Parameters
+    #__init__: Funcao chamada ao criar um instancia
+	# da classe RoboticArmAL5D. Recebe como parametros 
+	# as propriedades do braco: Numeros correspondentes
+	# aos servos e respectivos limites
     def __init__(self,properties):
         self.SERIAL_PORT='/dev/ttyS0' #Serial
         self.BAUD_RATE='115200'       #Set Baud rates to 115200
@@ -62,7 +66,7 @@ class RoboticArmAL5D:
             os.system('sudo rm /dev/ttyS0')  
             os.system('sudo ln -s /dev/ttyUSB0 /dev/ttyS0')   
         except IOError as e:
-            print ('SETUP - Erro de E/S({0}): {1}'.format(e.errno, e.strerror))
+            print ('Erro de E/S({0}): {1}'.format(e.errno, e.strerror))
         except:
             print ('Erro inesperado. Verifique se o braço está ligado e conectado ao computador.', sys.exc_info()[0])
             
@@ -79,13 +83,13 @@ parity=self.PARITY,
 xonxoff = self.XONXOFF,
 rtscts= self.RTSCTS)
             self.open_port()
-            print ('Porta serial /dev/ttyS0 aberta com sucesso\n')
+            #print ('Porta serial /dev/ttyS0 aberta com sucesso\n')
             return 1
         except IOError as e:
-            print ('OPEN PORTA '+e.strerror)
+            #print (e.strerror)
             return -1
         except:
-            print ('Erro abrindo a porta serial /dev/ttyS0\nAbortando o programa...\n')
+            #print ('Erro abrindo a porta serial /dev/ttyS0\nAbortando o programa...\n')
             return -1
 
     #3. Open serial port    
@@ -101,13 +105,17 @@ rtscts= self.RTSCTS)
     
     #5 Write 
     def envia_comando(self,cmd):
-        n = self.serial_port.write(cmd)
-        if n < 0:
-            n = -1
-            return n
-        else:
-            return n
+        self.serial_port.write('%s\r' % (cmd))
+#        if n < 0:
+#            n = -1
+#            return n
+#        else:
+#            return n
     
+    #trava: Travas de seguranca. Recebe como parametro
+	#o canal, isto e, o servo que se deseja comunicar
+	#e a posicao desejada. Testa a posicao de acordo
+	#com os limites pre-estabelecidos para o servo
     def trava(self,channel,pos):
         #BAS_SERVO
         if channel == self.BAS_SERVO:
@@ -166,5 +174,5 @@ rtscts= self.RTSCTS)
         time.sleep(2) # Entre 1.5s a 2s
         self.serial_port.write('#0P1500#1P1500#2P1500#3P1500#4P1500T1500\r')
         time.sleep(2)
-        self.envia_comando('#0P2500T1500\r')
+        self.envia_comando('#0P1000T200')
         
