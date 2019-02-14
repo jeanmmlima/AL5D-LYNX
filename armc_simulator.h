51a9516c8802dbbb7a2bd144df341f799a95065a
jeanmmlima <jeanmariomlima@gmail.com>
2019-02-14 16:54:58 -0300

    changes in header

--- armc_simulator.h
+++ armc_simulator.h
@@ -13,11 +13,13 @@ DEVELOPED BY: Eng. Msc. Jean M. M. Lima.
 #ifndef _ARMC_SIMULATOR_H_
 #define _ARMC_SIMULATOR_H_
 
-	#include "remoteApi/extApi.h"
+	#include "extApi.h"
 	#include <stdio.h>
 	#include <string.h>
 	#include <math.h>
 	#include <stdbool.h>
+	#include "v_repConst.h"
+	#include "v_repLib.h"
 
 	#define HOME_POS "#0P1500#1P1500#2P1500#3P1500#4P1500"
 
@@ -54,5 +56,6 @@ DEVELOPED BY: Eng. Msc. Jean M. M. Lima.
 	int simulacao_ativa(int);
 	void envia_comando(int,char[]);
 	float getPos(int,int);
+
 
 #endif
